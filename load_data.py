78f4844d50af498831f7ddcee4bf9b2fe58d5081
Ivayla <ivayla.stefanova@ru.nl>
2026-01-29 18:47:42 +0100

    update with borda

--- load_data.py
+++ load_data.py
@@ -1,5 +1,8 @@
 import os
+import numpy as np
 from preflibtools.instances import OrdinalInstance
+from collections import Counter
+from borda_rule import borda_rule
 
 def load_preflib_soi(file_path):
     base_path = os.path.dirname(os.path.abspath(__file__))
@@ -16,6 +19,22 @@ def load_preflib_soi(file_path):
     
     return clean_ballots, instance.num_alternatives
 
+def get_truncation_distribution(ballots):
+    lengths = [len(b) for b in ballots]
+    counts = Counter(lengths)
+    total = len(ballots)
+    dist = {length: count / total for length, count in counts.items()}
+    return dist
+
 #example
 ballots, num_cands = load_preflib_soi("debian/00002-00000001.soi")
 print(f"Success! Loaded {len(ballots)} ballots.")
+
+# calculate ballot length
+dist = get_truncation_distribution(ballots)
+print(f"Length distribution: {dist}")
+
+# borda
+winners, scores = borda_rule(ballots)
+print(f"True Winner(s): {winners}")
+print(f"Scores: {scores}")
